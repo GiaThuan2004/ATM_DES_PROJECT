bad8af336e6ef4a93aa814af4b12adf2e831e5a4
kimphu <phu.hhk05004@sinhvien.hoasen.edu.vn>
2024-10-07 14:52:52 +0700

    connect server to client

--- server/index.js
+++ server/index.js
@@ -3,22 +3,31 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 const {connectMongoDB} = require('./databases/mongoDb/connectMongoDB');
 
 require('dotenv').config();
 
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Kết nối MongoDB
 connectMongoDB();
+
 app.use('/', require('./routes/postTransactionRoute'));
 
+// Sử dụng các routes
+app.use('/api', require('./routes/postTransactionRoute'));
 
 // Đường dẫn đến file HTML
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
 
 // Khởi động server
 app.listen(port, () => {
